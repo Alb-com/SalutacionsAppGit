09127095b1acba4600746e32383a36130d548bf1
Albert Comas <albertcomaas@gmail.com>
2026-02-25 16:48:43 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SalutacionsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BonDia bonDia = new BonDia();
+            BonaNit bonaNit = new BonaNit();
+
+            bonDia.Saludar();
+            bonaNit.Saludar();
+        }
+    }
+}
